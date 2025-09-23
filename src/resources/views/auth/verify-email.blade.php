93ee25167374fb8dec644bd9d5086874072d96be
yusuke-yano-01 <kai0330m@gmail.com>
2025-09-23 09:12:45 +0900

    style(verify-email): remove card frame and heading; add responsive resend button styles; center content

--- src/resources/views/auth/verify-email.blade.php
+++ src/resources/views/auth/verify-email.blade.php
@@ -6,27 +6,20 @@
 
 @section('content')
 <div class="register-form__content">
-  <div class="register-form__heading">
-    <h2>メール認証が必要です</h2>
-  </div>
-
   @if (session('status') == 'verification-link-sent')
     <div class="login-error">
       <div class="login-error__message">新しい認証リンクを送信しました。</div>
     </div>
   @endif
 
-  <div class="form">
-    <p class="verification-message">
-      ご登録のメールアドレスに認証リンクを送信しました。<br>
-      メール内のリンクをクリックしてアカウントを認証してください。
-    </p>
-    <div class="form__button">
-      <form method="POST" action="{{ route('verification.send') }}">
-        @csrf
-        <button type="submit" class="form__button-submit">認証メールを再送信</button>
-      </form>
-    </div>
-  </div>
+  <p class="verify-message">
+    ご登録のメールアドレスに認証リンクを送信しています。<br>
+    メール内のリンクをクリックしてアカウントを認証してください。
+  </p>
+
+  <form method="POST" action="{{ route('verification.send') }}" class="verify-resend-form">
+    @csrf
+    <button type="submit" class="verify-button">認証メールを再送信</button>
+  </form>
 </div>
 @endsection
